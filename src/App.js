f72cab3922f445db3d50d8b44075f4403d9a9372
AlexaNem0 <vikalyeksyeyev@gmail.com>
2022-12-03 22:10:28 -0600

    hero completed

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import CoinDetails from "./components/CoinDetails";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
@@ -6,6 +7,7 @@ function App() {
     <>
       <Navbar />
       <Hero />
+      <CoinDetails />
     </>
   );
 }
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,8 +1,28 @@
 import React from "react";
 import "./Hero.css";
+import Crypto from "../assets/hero-img.png";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div className="hero">
+      <div className="container">
+        <div className="left">
+          <p> Buy & Sell Crypto 24/7 using your savings account</p>
+          <h1>Invest in Cryptocurrency with your TFSA</h1>
+          <p>Buy, Sell, and store hundreds of Coins in one place</p>
+          <div className="input-container">
+            <input type="text" placeholder="Enter your email" />
+            <button className="btn">Learn More</button>
+          </div>
+        </div>
+        <div className="right">
+          <div className="img-container">
+            <img src={Crypto} alt="Crypto" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
